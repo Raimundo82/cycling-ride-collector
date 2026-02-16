4935b88f3b967876a391962d03b00d1678051a88
João Raimundo <joao.raimundo.1982@gmail.com>
2026-02-16 18:40:17 +0000

    tests: add tests to token isExpired

---
+++ internal/infrastructure/auth/model/token_test.go
@@ -0,0 +1,78 @@
+package model
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestIsExpiredReturnFalseIfTokenIsNotExpired(t *testing.T) {
+	Convey("Given a token that expires in more than one minute", t, func() {
+		token := &Token{
+			AccessToken:  "test-access-token",
+			ExpiresAt:    time.Now().Add(2 * time.Minute),
+			RefreshToken: "test-refresh-token",
+		}
+
+		Convey("Then it returns the expected token", func() {
+			So(token.IsExpired(), ShouldBeFalse)
+		})
+	})
+}
+
+func TestIsExpiredReturnFalseIfTokenIsExpiringInOneMinute(t *testing.T) {
+	Convey("Given a token that expires in exactly one minute onde second", t, func() {
+		token := &Token{
+			AccessToken:  "test-access-token",
+			ExpiresAt:    time.Now().Add(1*time.Minute + 1*time.Second),
+			RefreshToken: "test-refresh-token",
+		}
+
+		Convey("Then it returns the expected token", func() {
+			So(token.IsExpired(), ShouldBeFalse)
+		})
+	})
+}
+
+func TestIsExpiredReturnTrueIfTokenIsExpiringInLessThanOneMinute(t *testing.T) {
+	Convey("Given a token that expires in less than one minute", t, func() {
+		token := &Token{
+			AccessToken:  "test-access-token",
+			ExpiresAt:    time.Now().Add(30 * time.Second),
+			RefreshToken: "test-refresh-token",
+		}
+
+		Convey("Then it returns the expected token", func() {
+			So(token.IsExpired(), ShouldBeTrue)
+		})
+	})
+}
+
+func TestIsExpiredReturnTrueIfTokenIsAboutToExpire(t *testing.T) {
+	Convey("Given a token that is about to expire", t, func() {
+		token := &Token{
+			AccessToken:  "test-access-token",
+			ExpiresAt:    time.Now(),
+			RefreshToken: "test-refresh-token",
+		}
+
+		Convey("Then it returns the expected token", func() {
+			So(token.IsExpired(), ShouldBeTrue)
+		})
+	})
+}
+
+func TestIsExpiredReturnTrueIfTokenIsZero(t *testing.T) {
+	Convey("Given a token with zero expiration time", t, func() {
+		token := &Token{
+			AccessToken:  "test-access-token",
+			ExpiresAt:    time.Time{},
+			RefreshToken: "test-refresh-token",
+		}
+
+		Convey("Then it returns the expected token", func() {
+			So(token.IsExpired(), ShouldBeTrue)
+		})
+	})
+}
