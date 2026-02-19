7a356d3e893951ce8d8c7d43eca2b41e7d0aa601
João Raimundo <joao.raimundo.1982@gmail.com>
2026-02-19 14:38:18 +0000

    refactor: update save workout period usecase to inject athlete data provider

--- internal/application/usecase/save_workout_period_test.go
+++ internal/application/usecase/save_workout_period_test.go
@@ -12,6 +12,7 @@ import (
 
 type mockWorkoutPeriodSaver struct {
 	Workouts      []*Workout
+	Athlete       *Athlete
 	SaveAllCalled int
 }
 
@@ -25,9 +26,18 @@ type mockPeriodWorkoutProvider struct {
 
 var _ contracts.WorkoutProvider = (*mockPeriodWorkoutProvider)(nil)
 
+type mockAthleteDataProvider struct {
+	AthleteData          *Athlete
+	GetAthleteDataCalled int
+	Err                  error
+}
+
+var _ contracts.AthleteDataProvider = (*mockAthleteDataProvider)(nil)
+
 // SaveAll implements [contracts.WorkoutRepository].
-func (m *mockWorkoutPeriodSaver) SaveAll(workouts []*Workout) error {
+func (m *mockWorkoutPeriodSaver) SaveAll(workouts []*Workout, athlete *Athlete) error {
 	m.Workouts = append(m.Workouts, workouts...)
+	m.Athlete = athlete
 	m.SaveAllCalled++
 	return nil
 }
@@ -41,8 +51,19 @@ func (m *mockPeriodWorkoutProvider) GetWorkoutsByPeriod(period Period) ([]*Worko
 	return m.Workouts, nil
 }
 
-func TestSaveWorkoutPeriod_GivenWorkoutsForPeriod_WhenExecute_ThenWorkoutsSaved(t *testing.T) {
-	Convey("Given workouts for the period from the provider", t, func() {
+// GetAthleteData implements [contracts.AthleteDataProvider].
+func (m *mockAthleteDataProvider) GetAthleteData() (*Athlete, error) {
+	m.GetAthleteDataCalled++
+	if m.Err != nil {
+		return nil, m.Err
+	}
+	return m.AthleteData, nil
+}
+
+func TestSaveWorkoutPeriodSavesWorkoutsAndAthleteData(t *testing.T) {
+	Convey("Given workouts for the period and an Athlete", t, func() {
+		a := NewAthlete(79, 135, 250)
+		athleteDataProvider := &mockAthleteDataProvider{AthleteData: a, Err: nil}
 		workoutProvider := &mockPeriodWorkoutProvider{
 			Workouts: []*Workout{
 				{ID: 1, StartTime: time.Date(2024, 6, 1, 10, 0, 0, 0, time.UTC), DurationInMin: 90},
@@ -56,7 +77,7 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForPeriod_WhenExecute_ThenWorkoutsSaved(
 
 		workoutRepo := &mockWorkoutPeriodSaver{Workouts: []*Workout{}, SaveAllCalled: 0}
 
-		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider)
+		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider, athleteDataProvider)
 
 		period, _ := NewPeriod(time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, 6, 3, 0, 0, 0, 0, time.UTC))
 
@@ -65,7 +86,9 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForPeriod_WhenExecute_ThenWorkoutsSaved(
 		Convey("When execute", func() {
 			err := saveWorkoutPeriod.Execute(period, minWorkoutDuration)
 
-			Convey("Then 3 workouts are saved", func() {
+			Convey("Then 3 workouts and athlete data are saved", func() {
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
+				So(workoutRepo.Athlete, ShouldResemble, a)
 				So(workoutProvider.GetWorkoutsByPeriodCalled, ShouldEqual, 1)
 				So(err, ShouldBeNil)
 				So(workoutRepo.SaveAllCalled, ShouldEqual, 1)
@@ -78,8 +101,10 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForPeriod_WhenExecute_ThenWorkoutsSaved(
 	})
 }
 
-func TestSaveWorkoutPeriod_GivenWorkoutsForOneDayPeriod_WhenExecute_ThenWorkoutIsSavedForOneDay(t *testing.T) {
-	Convey("Given workouts for one day period from the provider", t, func() {
+func TestSaveWorkoutPeriodGivenWorkoutsForOneDayPeriodWhenExecuteThenWorkoutIsSavedForOneDay(t *testing.T) {
+	Convey("Given workouts for one day and an Athlete", t, func() {
+		a := NewAthlete(79, 135, 250)
+		athleteDataProvider := &mockAthleteDataProvider{AthleteData: a, Err: nil}
 		workoutProvider := &mockPeriodWorkoutProvider{
 			Workouts: []*Workout{
 				{ID: 1, StartTime: time.Date(2024, 6, 1, 10, 0, 0, 0, time.UTC), DurationInMin: 90},
@@ -90,7 +115,7 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForOneDayPeriod_WhenExecute_ThenWorkoutI
 
 		workoutRepo := &mockWorkoutPeriodSaver{Workouts: []*Workout{}, SaveAllCalled: 0}
 
-		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider)
+		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider, athleteDataProvider)
 
 		period, _ := NewPeriod(time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC))
 
@@ -99,8 +124,11 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForOneDayPeriod_WhenExecute_ThenWorkoutI
 		Convey("When execute", func() {
 			err := saveWorkoutPeriod.Execute(period, minWorkoutDuration)
 
-			Convey("Then 1 workout is saved", func() {
+			Convey("Then the workout and athlete data are saved", func() {
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
+				So(workoutRepo.Athlete, ShouldResemble, a)
 				So(workoutProvider.GetWorkoutsByPeriodCalled, ShouldEqual, 1)
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
 				So(err, ShouldBeNil)
 				So(workoutRepo.SaveAllCalled, ShouldEqual, 1)
 				So(workoutRepo.Workouts, ShouldHaveLength, 1)
@@ -110,11 +138,13 @@ func TestSaveWorkoutPeriod_GivenWorkoutsForOneDayPeriod_WhenExecute_ThenWorkoutI
 	})
 }
 
-func TestSaveWorkoutPeriod_GivenNoWorkouts_WhenExecute_ThenRestWorkoutsAreSaved(t *testing.T) {
-	Convey("Given no workouts for the period from the provider", t, func() {
+func TestSaveWorkoutPeriodGivenNoWorkoutsWhenExecuteThenRestWorkoutsAreSaved(t *testing.T) {
+	Convey("Given no workouts and an Athlete", t, func() {
+		a := NewAthlete(79, 135, 250)
+		athleteDataProvider := &mockAthleteDataProvider{AthleteData: a, Err: nil}
 		workoutProvider := &mockPeriodWorkoutProvider{Workouts: []*Workout{}, GetWorkoutsByPeriodCalled: 0}
 		workoutRepo := &mockWorkoutPeriodSaver{Workouts: []*Workout{}, SaveAllCalled: 0}
-		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider)
+		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider, athleteDataProvider)
 		period, _ := NewPeriod(time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, 6, 7, 0, 0, 0, 0, time.UTC))
 		minWorkoutDuration := 30
 
@@ -122,6 +152,8 @@ func TestSaveWorkoutPeriod_GivenNoWorkouts_WhenExecute_ThenRestWorkoutsAreSaved(
 			err := saveWorkoutPeriod.Execute(period, minWorkoutDuration)
 
 			Convey("Then rest workouts are saved", func() {
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
+				So(workoutRepo.Athlete, ShouldResemble, a)
 				So(workoutProvider.GetWorkoutsByPeriodCalled, ShouldEqual, 1)
 				So(err, ShouldBeNil)
 				So(workoutRepo.SaveAllCalled, ShouldEqual, 1)
@@ -131,11 +163,13 @@ func TestSaveWorkoutPeriod_GivenNoWorkouts_WhenExecute_ThenRestWorkoutsAreSaved(
 	})
 }
 
-func TestSaveWorkoutPeriod_GivenProviderError_WhenExecute_ThenReturnsError(t *testing.T) {
-	Convey("Given error from the provider", t, func() {
+func TestSaveWorkoutPeriodGivenActivityProviderErrorWhenExecuteThenReturnsError(t *testing.T) {
+	Convey("Given error from the activity provider", t, func() {
+		a := NewAthlete(79, 135, 250)
+		athleteDataProvider := &mockAthleteDataProvider{AthleteData: a, Err: nil}
 		workoutProvider := &mockPeriodWorkoutProvider{Workouts: []*Workout{}, GetWorkoutsByPeriodCalled: 0, Err: errors.New("provider error")}
 		workoutRepo := &mockWorkoutPeriodSaver{Workouts: []*Workout{}, SaveAllCalled: 0}
-		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider)
+		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider, athleteDataProvider)
 		period, _ := NewPeriod(time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, 6, 7, 0, 0, 0, 0, time.UTC))
 		minWorkoutDuration := 30
 
@@ -147,6 +181,34 @@ func TestSaveWorkoutPeriod_GivenProviderError_WhenExecute_ThenReturnsError(t *te
 				So(err, ShouldNotBeNil)
 				So(workoutRepo.SaveAllCalled, ShouldEqual, 0)
 				So(workoutRepo.Workouts, ShouldHaveLength, 0)
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
+				So(workoutRepo.Athlete, ShouldBeNil)
+				So(err.Error(), ShouldContainSubstring, "provider error")
+			})
+		})
+	})
+}
+
+func TestSaveWorkoutPeriodGivenAthleteProviderErrorWhenExecuteThenReturnsError(t *testing.T) {
+	Convey("Given error from the athlete provider", t, func() {
+		a := NewAthlete(79, 135, 250)
+		athleteDataProvider := &mockAthleteDataProvider{AthleteData: a, Err: errors.New("provider error")}
+		workoutProvider := &mockPeriodWorkoutProvider{Workouts: []*Workout{}, GetWorkoutsByPeriodCalled: 0}
+		workoutRepo := &mockWorkoutPeriodSaver{Workouts: []*Workout{}, SaveAllCalled: 0}
+		saveWorkoutPeriod := NewSaveWorkoutPeriod(NewLongestWorkout(), workoutRepo, workoutProvider, athleteDataProvider)
+		period, _ := NewPeriod(time.Date(2024, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2024, 6, 7, 0, 0, 0, 0, time.UTC))
+		minWorkoutDuration := 30
+
+		Convey("When execute", func() {
+			err := saveWorkoutPeriod.Execute(period, minWorkoutDuration)
+
+			Convey("Then Error is returned", func() {
+				So(workoutProvider.GetWorkoutsByPeriodCalled, ShouldEqual, 0)
+				So(err, ShouldNotBeNil)
+				So(workoutRepo.SaveAllCalled, ShouldEqual, 0)
+				So(workoutRepo.Workouts, ShouldHaveLength, 0)
+				So(athleteDataProvider.GetAthleteDataCalled, ShouldEqual, 1)
+				So(workoutRepo.Athlete, ShouldBeNil)
 			})
 		})
 	})
